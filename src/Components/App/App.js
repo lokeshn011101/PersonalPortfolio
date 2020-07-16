443227482b149899b5ab2a71816807bed42a3195
kaliboy011101@gmail.com <lokesh19055@cse.ss.edu.in>
2020-07-16 19:52:30 +0530

    made changes to css p2

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,30 +6,20 @@ import Skills from "../Skills/Skills";
 import Projects from "../Projects/Projects";
 import Contact from "../Contact/Contact";
 import Navbar from "../Navbar/Navbar";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <div className="appcontainer">
-          <section className="navbar">
-            <Navbar />
-          </section>
-          <section className="home">
-            <Home />
-          </section>
-          <section className="skills">
-            <Skills />
-          </section>
-          <section className="projects">
-            <Projects />
-          </section>
-          <section className="contact">
-            <Contact />
-          </section>
-        </div>
-      </HashRouter>
+      <div className="appcontainer">
+        <BrowserRouter>
+          <Navbar />
+          <Home />
+          <Skills />
+          <Projects />
+          <Contact />
+        </BrowserRouter>
+      </div>
     );
   }
 }
