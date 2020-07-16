7f5dd3b515fa8fddeb18863c1699d21f127423fa
kaliboy011101@gmail.com <lokesh19055@cse.ss.edu.in>
2020-07-16 19:19:24 +0530

    Updates

---
+++ precache-manifest.9272be190fd7fca58f35a369d214ec31.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12119f6f24259b8dc4c861ce6b035dce",
+    "url": "/PersonalPortfolio/index.html"
+  },
+  {
+    "revision": "1e3361915829209cfdbe",
+    "url": "/PersonalPortfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "e754473c1cdabba3b8af",
+    "url": "/PersonalPortfolio/static/css/main.474d6bec.chunk.css"
+  },
+  {
+    "revision": "1e3361915829209cfdbe",
+    "url": "/PersonalPortfolio/static/js/2.19e5b72f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PersonalPortfolio/static/js/2.19e5b72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e754473c1cdabba3b8af",
+    "url": "/PersonalPortfolio/static/js/main.2ca162e7.chunk.js"
+  },
+  {
+    "revision": "b3a7d4e889e2e3739183",
+    "url": "/PersonalPortfolio/static/js/runtime-main.01cb99b5.js"
+  },
+  {
+    "revision": "fcba1fce8789ffbe8a0332d682530064",
+    "url": "/PersonalPortfolio/static/media/222.fcba1fce.png"
+  },
+  {
+    "revision": "89b607f1b49f977a2c025cbc5b05239c",
+    "url": "/PersonalPortfolio/static/media/One.89b607f1.png"
+  },
+  {
+    "revision": "42e9a1f312b7bf5d3f2ab529e75f35b9",
+    "url": "/PersonalPortfolio/static/media/Pandas.42e9a1f3.png"
+  },
+  {
+    "revision": "3768b1037695ffeca4cf34779e880f4b",
+    "url": "/PersonalPortfolio/static/media/Three.3768b103.png"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/PersonalPortfolio/static/media/c++.8f42e018.png"
+  },
+  {
+    "revision": "1c1888a3eb2f04360763c1a95257b726",
+    "url": "/PersonalPortfolio/static/media/c.1c1888a3.png"
+  },
+  {
+    "revision": "c9421be52fe48fdf04839b53f8e9f1d4",
+    "url": "/PersonalPortfolio/static/media/css.c9421be5.png"
+  },
+  {
+    "revision": "d3304b833b96ca9b6b4d4f12d07d7176",
+    "url": "/PersonalPortfolio/static/media/javascript.d3304b83.png"
+  },
+  {
+    "revision": "18792f6740a187336ab958b8b68c4f46",
+    "url": "/PersonalPortfolio/static/media/myphoto.18792f67.JPG"
+  },
+  {
+    "revision": "efb91bd6cd3d5e8b2ea4e178bc63f3ce",
+    "url": "/PersonalPortfolio/static/media/numpy.efb91bd6.png"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/PersonalPortfolio/static/media/python.b9bbb1d7.png"
+  }
+]);
