7dd384e4f09af15a679c78ac1a19e8251b0299d0
kaliboy011101@gmail.com <lokesh19055@cse.ss.edu.in>
2020-07-16 20:17:24 +0530

    made changes to css p4

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -13,53 +13,59 @@ export default class Contact extends React.Component {
         <div className="cheading" data-aos="fade-down">
           <h1>So You've come this far, Why don't we talk!?</h1>
         </div>
-        <div className="clinks" data-aos="fade-left" data-aos-delay="180">
-          <div className="l11">
-            <a
-              href="https://www.linkedin.com/in/lokesh-n-n-47a294190/"
-              className="linked a1"
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <div className="span1">
-                Linked<div id="span1">I</div>N
-              </div>
-            </a>
+        <div className="clinks">
+          <div data-aos="fade-left" data-aos-delay="180">
+            <div className="l11">
+              <a
+                href="https://www.linkedin.com/in/lokesh-n-n-47a294190/"
+                className="linked a1"
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <div className="span1">
+                  Linked<div id="span1">I</div>N
+                </div>
+              </a>
+            </div>
           </div>
 
-          <div className="l11" data-aos="fade-down" data-aos-delay="200">
-            <a
-              href="https://www.instagram.com/_being_lokesh_/"
-              className="insta a1"
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <div className="span2">Instagram</div>
-            </a>
+          <div data-aos="fade-down" data-aos-delay="200">
+            <div className="l11">
+              <a
+                href="https://www.instagram.com/_being_lokesh_/"
+                className="insta a1"
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <div className="span2">Instagram</div>
+              </a>
+            </div>
           </div>
-
-          <div className="l11" data-aos="fade-up" data-aos-delay="225">
-            <a href="/" className="fb a1">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <div id="span3">Facebook</div>
-            </a>
+          <div data-aos="fade-up" data-aos-delay="225">
+            <div className="l11">
+              <a href="/" className="fb a1">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <div id="span3">Facebook</div>
+              </a>
+            </div>
           </div>
-
-          <div className="l11" data-aos="fade-right" data-aos-delay="250">
-            <a href="https://github.com/lokeshn011101" className="github a1">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <div id="span4">GitHub</div>
-            </a>
+          <div data-aos="fade-right" data-aos-delay="250">
+            <div className="l11">
+              <a href="https://github.com/lokeshn011101" className="github a1">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <div id="span4">GitHub</div>
+              </a>
+            </div>
           </div>
         </div>
         <div className="concon">
